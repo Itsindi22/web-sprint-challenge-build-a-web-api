618f70f6558ad0bebe8ea34fd46d8c6ce0ab926c
Inderpreet <inderpreet2004123@gmail.com>
2025-09-29 12:19:24 -0400

    start server

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,32 @@
 // Write your "projects" router here!
+const express =require ('express');
+const  projects = require ('./projects-model')
+
+const router = express.Router()
+
+router.get('/api/projects',(req,res) => {
+    projects.find()
+    .then(found => {
+        res.json(found)
+    })
+    .catch (err => {
+      res.status(500).json({
+        message:"The points information could not be retrieved",
+        err: err.message,
+        stack: err.stack,
+      })
+        })
+    })
+
+
+module.exports = router
+
+
+
+
+
+
+
+
+
+
